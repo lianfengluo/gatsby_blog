08911a65425a22cbb6a754354e39da3a1ef8df9c
Richard Luo <richard.luo@hivery.com>
2020-09-01 01:52:34 +1000

    Adding and config google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,5 +43,15 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: 'swap',
+      },
+    },
   ],
 };
