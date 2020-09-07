8a03b02827527dca99bce85998f0d33799e9f340
Richard Luo <richard.luo@hivery.com>
2020-09-07 18:22:30 +1000

    Adding extension

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
+        extensions: [`.md`, `.mdx`],
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-images`,
