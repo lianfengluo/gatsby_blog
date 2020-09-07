72d02c7b794a451bad31b2a60c856f34450166f0
Richard Luo <richard.luo@hivery.com>
2020-09-07 18:16:04 +1000

    Remove ts node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,5 @@
-'use strict';
-
-/**
- * Source-map-support mimics node's stack trace making debugging easier
- * ts-node register helps importing and compiling TypeScript modules into JS
- */
-require('source-map-support').install();
-require('ts-node').register();
+// require('source-map-support').install();
+// require('ts-node').register();
 const path = require(`path`);
 
 exports.createPages = async ({ graphql, actions }) => {
